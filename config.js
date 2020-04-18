d5fffd4ea1aaa49facc79a70024742aa584f5cf6
Preston Neal <preston@afcamail.com>
2020-04-17 23:25:27 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  bucket: {
+    slug: process.env.COSMIC_BUCKET || 'seans-blog',
+    read_key: process.env.COSMIC_READ_KEY || 'zDHv1hhIkR96LLUKZVBhKlqU2SefN0ufokDPRtUDn25B4dSFrQ'
+  }
+}
